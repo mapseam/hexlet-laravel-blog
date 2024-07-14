890d19484aeac1d0dfc8e53e096ad86b9676fcab
Oleg Kartashov <mapseam@yandex.ru>
2024-07-14 09:07:47 +0300

    Use layout App for view About

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,2 +1,10 @@
-<h1>О блоге</h1>
-<p>Эксперименты с Laravel на Хекслете</p>
+@extends('layouts.app')
+
+section('title', 'О блоге');
+
+section('header', 'О блоге');
+
+@section('content')
+    <h1>О блоге</h1>
+    <p>Эксперименты с Laravel на Хекслете</p>
+@endsection
